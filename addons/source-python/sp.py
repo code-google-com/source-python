f7622301850567914ad0d045de432472805f60e1
Deniz Sezen <thinkerroad@gmail.com>
2012-07-12 21:26:57 -0400

    * Added VDF file. * Added core sp.py file.

---
+++ addons/source-python/sp.py
@@ -0,0 +1,48 @@
+#
+# =============================================================================
+# Source Python
+# Copyright (C) 2012 Source Python Development Team.  All rights reserved.
+# =============================================================================
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License, version 3.0, as published by the
+# Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# As a special exception, the Source Python Team gives you permission 
+# to link the code of this program (as well as its derivative works) to 
+# "Half-Life 2," the "Source Engine," and any Game MODs that run on software
+# by the Valve Corporation.  You must obey the GNU General Public License in
+# all respects for all other code used.  Additionally, the Eventscripts
+# Development Team grants this exception to all derivative works.  
+#
+
+# =============================================================================
+# Imports
+# =============================================================================
+
+# =============================================================================
+# Core functions.
+# =============================================================================
+def addon_load( addon_name ):
+    ''' Called when a user executes sp_load. '''
+    pass
+    
+def addon_unload( addon_name ):
+    ''' Called when a user executes sp_unload. '''
+    pass
+    
+def addon_reload( addon_name ):
+    ''' Called when a user executes sp_reload. '''
+    pass
+    
+def event_fire( game_event ):
+    ''' Called when the core catches an event. '''
+    pass
