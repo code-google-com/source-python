8dc48e7570c070d714d2d6ab623e8aa7aff6e3fb
Deniz Sezen <thinkerroad@gmail.com>
2012-08-07 19:13:07 -0400

    core/sp_python.cpp: * Added site-packages folder to path.

--- src/core/core/sp_python.cpp
+++ src/core/core/sp_python.cpp
@@ -103,6 +103,15 @@ bool CPythonManager::Initialize( void )
 	V_snprintf(szPlatCmd, MAX_GAME_PATH, "sys.path.append(r\"%s\")", szPlatDir);
 	PyRun_SimpleString(szPlatCmd);
 
+    // Add the site packages directory.
+	char szSitePackages[MAX_GAME_PATH];
+	V_snprintf(szSitePackages, MAX_GAME_PATH, "%s/engines/site-packages", g_GamePaths.GetESDir());
+	V_FixSlashes(szSitePackages);
+    
+    char szSitePackagesCmd[MAX_GAME_PATH];
+    V_snprintf(szSitePackagesCmd, MAX_GAME_PATH, "sys.path.append(r\"%s\")", szSitePackages);
+    PyRun_SimpleString(szSitePackagesCmd);
+    
 	// Add the addons directory too.
 	char szAddonsCmd[MAX_GAME_PATH];
 	V_snprintf(szAddonsCmd, MAX_GAME_PATH, "sys.path.append(r\"%s\")", g_GamePaths.GetESDir());
