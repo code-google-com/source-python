d9cb56899429e09120dbd705a8a0feb798eeccba
Stephen Toon <satoon101@gmail.com>
2012-08-12 12:31:51 -0400

    * Added "all" player filter to return all indexes * _BaseFilterManager now passes the classname to _BaseRegistry so the errors show the proper class name that registered

---
+++ addons/source-python/_libs/events/decorator.py
@@ -0,0 +1,45 @@
+# ../_libs/events/decorator.py
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Core
+from core.decorators import BaseDecorator
+#   Events
+from events.manager import EventRegistry
+
+
+# =============================================================================
+# >> CLASSES
+# =============================================================================
+class Event(BaseDecorator):
+    '''Event decorator class'''
+
+    def __init__(self, callback):
+        '''Store the callback and register the event'''
+
+        # Is the callback callable?
+        if not callable(callback):
+
+            # Raise an error
+            raise TypeError(
+                "'" + type(callback).__name__ + "' object is not callable")
+
+        # Store the callback
+        self.callback = callback
+
+        # Register the event
+        EventRegistry.RegisterForEvent(self.callback.__name__, self.callback)
+
+    def __call__(self, GameEvent):
+        '''Calls the Event callback with the GameEvent instance'''
+
+        # Call the Event callback
+        return self.callback(GameEvent)
+
+    def _UnregisterDecorator(self):
+        '''Unregisters the event'''
+
+        # Unregister the event
+        EventRegistry.UnregisterForEvent(self.callback.__name__, self.callback)
---
+++ addons/source-python/sp.py
@@ -0,0 +1,166 @@
+# ../sp.py
+
+'''
+    ===========================================================================
+    Source Python
+    Copyright (C) 2012 Source Python Development Team.  All rights reserved.
+    ===========================================================================
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License, version 3.0, as
+    published by the Free Software Foundation.
+    
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    As a special exception, the Source.Python Team gives you permission 
+    to link the code of this program (as well as its derivative works) to 
+    "Half-Life 2," the "Source Engine," and any Game MODs that run on software
+    by the Valve Corporation.  You must obey the GNU General Public License in
+    all respects for all other code used.  Additionally, the Source.Python
+    Development Team grants this exception to all derivative works.  
+'''
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Addons
+from addons.info import AddonInfo
+from addons.manager import AddonManager
+#   Events
+from events.manager import EventRegistry
+
+
+# =============================================================================
+# >> CORE FUNCTIONS
+# =============================================================================
+def addon_load(addon_name):
+    '''Called when a user executes sp_load.'''
+
+    # Is an addon being loaded?
+    if not addon_name:
+
+        # Print start message for loaded addons
+        print('[SP] Loaded Addons:')
+        print('======================================\n')
+
+        # Loop through all loaded addons
+        for addon in AddonManager:
+
+            # Set info as None in case no AddonInfo instance is found
+            info = None
+
+            # Loop through the addon's globals
+            for object_name in AddonManager[addon].globals:
+
+                # Get the object's instance
+                instance = AddonManager[addon].globals[object_name]
+
+                # Is the current instance an AddonInfo instance?
+                if isinstance(instance, AddonInfo):
+
+                    # Set info to the instance
+                    info = instance
+
+                    # Break the loop
+                    break
+
+            # Does the addon have an AddonInfo instance?
+            if not info is None:
+
+                # Print the addon's name
+                print(addon + ':')
+
+                # Loop through all items in the AddonInfo instance
+                for item in info:
+
+                    # Print the item's name
+                    print('\t%s:' % item)
+
+                    # Print the item's value
+                    print('\t\t%s' % info[item])
+
+            # Does the addon not have an AddonInfo instance?
+            else:
+
+                # Print the addon's name
+                print(addon)
+
+            # Print a blank line between addons
+            print('\n')
+
+        # Print closing message for loaded addons
+        print('======================================')
+
+        # No need to go further
+        return
+
+    # Is the addon already loaded?
+    if addon_name in AddonManager:
+
+        # Print message that the addon is already loaded
+        print('[SP] Addon "%s" is already loaded.' % addon_name)
+
+        # No need to go further
+        return
+
+    # Get the addon's instance
+    addon = AddonManager[addon_name]
+
+    # Is the addon loaded?
+    if addon is None:
+
+        # Print a message that the addon was not able to be loaded
+        print('[SP] Addon "%s" was unable to be loaded.' % addon_name)
+
+        # If not, go no further
+        return
+
+    # Print message that the addon successfully loaded
+    print('[SP] Successfully loaded "%s"' % addon_name)
+
+
+def addon_unload(addon_name):
+    '''Called when a user executes sp_unload.'''
+
+    # Is the loaded?
+    if not addon_name in AddonManager:
+
+        # Print message that the addon is not loaded
+        print('[SP] Addon "%s" cannot ' % addon_name +
+            'be unloaded.  It is not currently loaded.')
+
+        # No need to go further
+        return
+
+    # Get the addon's instance
+    addon = AddonManager[addon_name]
+
+    # Remove the addon from the AddonManager
+    del AddonManager[addon_name]
+
+    # Print message that the addon successfully unloaded
+    print('[SP] Successfully unloaded "%s"' % addon_name)
+
+
+def addon_reload(addon_name):
+    '''Called when a user executes sp_reload.'''
+
+    # Unload the addon
+    addon_unload(addon_name)
+
+    # Load the addon
+    addon_load(addon_name)
+
+
+def event_fire(GameEvent):
+    '''Called when the core catches an event.'''
+
+    # Call the event within the registry
+    EventRegistry.CallEventCallbacks(GameEvent)
