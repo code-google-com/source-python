15b8e724a6b981403c8bb9b002cf82dbf55631ea
Stephen Toon <satoon101@gmail.com>
2012-08-27 20:52:50 -0400

    * More changes due to path constants being updated to use the "path" module

--- addons/source-python/_libs/messages/_base.py
+++ addons/source-python/_libs/messages/_base.py
@@ -4,25 +4,22 @@
 # >> IMPORTS
 # =============================================================================
 # Python Imports
-#   OS
-from os.path import isfile
-from os.path import join
 #   ConfigObj
 from configobj import ConfigObj
 
 # Source.Python Imports
 from core import GAME_NAME
-from paths import CFG_PATH
+from paths import DATA_PATH
 
 
 # =============================================================================
 # >> GLOBAL VARIABLES
 # =============================================================================
 # Get the current game's message types
-_file_path = join(CFG_PATH, '_libs', 'messages', '%s.ini' % GAME_NAME)
+_file_path = DATA_PATH.join('messages', GAME_NAME + '.ini')
 
 # Does the file exist?
-if not isfile(_file_path):
+if not _file_path.isfile()
 
     # If not, raise an error that the Messaging libraries are not set for game
     raise NotImplementedError('Source.Python messaging ' +
