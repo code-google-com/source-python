e181da308c7d1e3cd64481da9d43591ed5719555
Stephen Toon <satoon101@gmail.com>
2012-12-04 11:32:30 -0500

    * Updated command registration
    * Added Client command registration

---
+++ addons/source-python/_libs/commands/base.py
@@ -0,0 +1,45 @@
+# ../_libs/commands/base.py
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Core
+from core.decorators import BaseDecorator
+
+
+# =============================================================================
+# >> CLASSES
+# =============================================================================
+class _CommandDecorator(BaseDecorator):
+    '''Decorator class used to register/unregister commands'''
+
+    def __init__(self, class_instance):
+        '''Stores the instanciating class' unregister method'''
+        self._command_called = class_instance._command_called
+        self._unregister_decorator = class_instance._unregister_command
+
+
+class CommandRegistration(object):
+    '''Base command registration class'''
+
+    def __call__(self, callback):
+        '''Registers the command to the given callback'''
+
+        # Store the callback
+        self.callback = callback
+
+        # Register the command
+        self._command = self._register_command()
+
+        self._decorator_instance = _CommandDecorator(self)
+
+        # Add the callback to the commands callback list
+        self._command.AddToEnd(self._decorator_instance._command_called)
+
+        # Return a decorator instance of this class
+        return self._decorator_instance
+
+    def _unregister_command(self):
+        '''Unregisters the callback from the command'''
+        self._command.Remove(self._decorator_instance._command_called)
