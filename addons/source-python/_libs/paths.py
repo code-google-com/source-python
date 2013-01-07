85d35450dd143edc72f94fcc68e6083136b57c96
Stephen Toon <satoon101@gmail.com>
2013-01-07 15:14:51 -0500

    * Added custom sound directory for Source.Python
    * Added custom sound directory to paths as SOUND_PATH

--- addons/source-python/_libs/paths.py
+++ addons/source-python/_libs/paths.py
@@ -30,5 +30,8 @@ TRANSLATION_PATH = GAME_PATH.joinpath(
 # Get the custom event's base path
 EVENT_PATH = GAME_PATH.joinpath('resource', 'source-python', 'events')
 
+# Get the sound's base path
+SOUND_PATH = GAME_PATH.joinpath('sound', 'source-python')
+
 # Get the data's base path
 DATA_PATH = ADDON_PATH.joinpath('_libs', '_data')
