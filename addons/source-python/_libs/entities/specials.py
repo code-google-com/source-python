45d49f53eac1a519afbad7dd4d648050bc31c29f
Stephen Toon <satoon101@gmail.com>
2013-01-14 12:22:50 -0500

    * Added new entity.constants module - currently stores damage types and offsets for CTakeDamageInfo values
    * Moved some imports out of the global scope in entities.specials as they were causing cross-import errors

--- addons/source-python/_libs/entities/specials.py
+++ addons/source-python/_libs/entities/specials.py
@@ -8,18 +8,18 @@ from Source import Binutils
 #   DynCall
 from dyncall.dictionary import SignatureDictionary
 #   Entities
-from entities.entity import BaseEntity
+from entities.constants import DamageOffsets
 from entities.helpers import index_from_inthandle
-from entities.weapons.weapon import WeaponEntity
 #   Filters
 from filters.weapontags import WeaponTagIter
-#   Players
+#   Weapons
 from weapons.errors import WeaponIndexError
 
 
 # =============================================================================
 # >> GLOBAL VARIABLES
 # =============================================================================
+# Get a list of projectiles for the game
 _projectile_weapons = list(WeaponTagIter('grenade', return_types='classname'))
 
 
@@ -29,8 +29,16 @@ _projectile_weapons = list(WeaponTagIter('grenade', return_types='classname'))
 class _EntitySpecials(object):
     '''Base class used to hold special functionality'''
 
-    def damage(self, victim_index, damage=0, damage_type=0, weapon_index=None):
+    def damage(
+        self, victim_index, damage=0, damage_type=0,
+        weapon_index=None, hitgroup=0, **keywords):
         '''Method used to hurt another entity with the given arguments'''
+
+        # Import BaseEntity classes
+        # Doing this in the global scope causes cross import errors
+        from entities.entity import BaseEntity
+        from entities.weapons.weapon import WeaponEntity
+        from players.entity import PlayerEntity
 
         # Is the game supported?
         if not 'TakeDamage' in SignatureDictionary:
@@ -38,9 +46,6 @@ class _EntitySpecials(object):
             # Raise an error if not supported
             raise NotImplementedError(
                 'damage is not implemented for %s' % GAME_NAME)
-
-        # Get the victim's BaseEntity instance.
-        victim = BaseEntity(victim_index)
 
         # Was no weapon index given?
         if weapon_index is None and self.classname == 'player':
@@ -61,6 +66,21 @@ class _EntitySpecials(object):
             raise WeaponIndexError(
                 'Invalid index "%s" for weapon' % weapon_index)
 
+        # Get the victim's BaseEntity instance.
+        victim = BaseEntity(victim_index)
+
+        # Is the victim a player?
+        if victim.classname == 'player':
+
+            # Get the victim's PlayerEntity instance instead
+            victim = PlayerEntity(victim_index)
+
+            # Is hitgroup a valid attribute?
+            if hasattr(victim, 'hitgroup'):
+
+                # Set the victim's hitgroup
+                victim.hitgroup = hitgroup
+
         # Get a memory address for CTakeDamageInfo
         take_damage_info = Binutils.AllocateMemory(96)
 
@@ -68,25 +88,43 @@ class _EntitySpecials(object):
         if weapon.classname in _projectile_weapons:
 
             # Set the hInflictor to the weapon's handle
-            Binutils.SetLocInt(take_damage_info + 36, weapon.handle.ToInt())
+            Binutils.SetLocInt(take_damage_info +
+                DamageOffsets.hInflictor, weapon.handle.ToInt())
 
         # Is the weapon not a projectile?
         else:
 
             # Set the hInflictor to the entity's handle
-            Binutils.SetLocInt(take_damage_info + 36, self.handle.ToInt())
+            Binutils.SetLocInt(take_damage_info +
+                DamageOffsets.hInflictor, self.handle.ToInt())
 
         # Set the hAttacker to the entity's handle
-        Binutils.SetLocInt(take_damage_info + 40, self.handle.ToInt())
+        Binutils.SetLocInt(take_damage_info +
+            DamageOffsets.hAttacker, self.handle.ToInt())
 
         # Set the hWeapon to the weapon's handle
-        Binutils.SetLocInt(take_damage_info + 44, weapon.handle.ToInt())
+        Binutils.SetLocInt(take_damage_info +
+            DamageOffsets.hWeapon, weapon.handle.ToInt())
 
         # Set the flDamage amount
-        Binutils.SetLocFloat(take_damage_info + 48, float(damage))
+        Binutils.SetLocFloat(take_damage_info +
+            DamageOffsets.flDamage, float(damage))
 
-        # Set the iDamageType value
-        Binutils.SetLocInt(take_damage_info + 60, damage_type)
+        # Set the bitsDamageType value
+        Binutils.SetLocInt(take_damage_info +
+            DamageOffsets.bitsDamageType, damage_type)
+
+        # Loop through the given keywords
+        for item in keywords:
+
+            # Is the keyword supported?
+            if item in DamageOffsets:
+
+                # Set the offset's value
+                getattr(
+                    Binutils, 'SetLoc%s' % DamageOffsets[item]['type'])(
+                    take_damage_info + DamageOffsets[item]['offset'],
+                    keywords[item])
 
         # Call the function with the victim's pointer and the CTakeDamageInfo
         SignatureDictionary['TakeDamage'].call_function(
