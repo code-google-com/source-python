b8f1588648c74c4f6342f899022790cf5f9a916e
Stephen Toon <satoon101@gmail.com>
2013-01-29 23:38:44 -0500

    * PEP8 standards fixes

--- addons/source-python/_libs/messages/types/shake.py
+++ addons/source-python/_libs/messages/types/shake.py
@@ -10,6 +10,7 @@ from core import GameEngine
 from messages.base import BaseMessageNoText
 from messages.base import MessageTypes
 
+
 # =============================================================================
 # >> CLASSES
 # =============================================================================
--- addons/source-python/_libs/translations/strings.py
+++ addons/source-python/_libs/translations/strings.py
@@ -28,9 +28,9 @@ from translations.manager import LanguageManager
 # >> GLOBAL VARIABLES
 # =============================================================================
 # Get an re.compile instance to correct all double escaped strings
-_double_escaped_pattern = re_compile(r'''(\\(?:(?P<octal>[0-7]{1,3})|
-    x(?P<hexadecimal>[0-9|a-f|A-F]{2})|(?P<notation>a|b|e|f|n|r|s|t|v)))''',
-    VERBOSE)
+_double_escaped_pattern = re_compile(
+    r'''(\\(?:(?P<octal>[0-7]{1,3})|x(?P<hexadecimal>[0-9|a-f|A-F]{2})|
+    (?P<notation>a|b|e|f|n|r|s|t|v)))''', VERBOSE)
 
 
 # =============================================================================
@@ -58,7 +58,7 @@ class LangStrings(dict):
 
         # Get the strings from the main file
         main_strings = ConfigObj(self._mainfile, encoding=encoding)
-        
+
         # Does the server specific file exist?
         if not self._serverfile.isfile():
 
@@ -67,7 +67,7 @@ class LangStrings(dict):
 
         # Otherwise
         else:
-            
+
             # Get any strings from the server specific file
             server_strings = ConfigObj(self._serverfile, encoding=encoding)
 
@@ -79,10 +79,10 @@ class LangStrings(dict):
 
             # Is the current string not a Section?
             if not isinstance(main_strings[key], Section):
-                
+
                 # No need to go further
                 continue
-                
+
             # Get a TranslationStrings instance for the current string
             translation_strings = TranslationStrings()
 
@@ -100,57 +100,58 @@ class LangStrings(dict):
                     continue
 
                 # Get the language's string and fix any escaped strings
-                translation_strings[language] = self._replace_escaped_sequences(
-                    main_strings[key][lang])
+                translation_strings[
+                    language] = self._replace_escaped_sequences(
+                        main_strings[key][lang])
 
             # Add the TranslationStrings instance for the current string
             self[key] = translation_strings
 
         # Is there any default language specified into the main file?
         if 'DEFAULT_LANGUAGE' in main_strings:
-            
+
             # Get the default language
             default_language = main_strings['DEFAULT_LANGUAGE']
-            
+
             # Make sure it is not a Section
             if not isinstance(default_language, Section):
-                
+
                 # Get the given language code
                 language_code = LanguageManager.get_language(default_language)
-                
+
                 # Is the language valid?
                 if not language_code is None:
-                    
+
                     # Set the default language
                     self.default_language = language_code
-                    
+
                 # Delete the key from the main file as we are done with it
                 del main_strings['DEFAULT_LANGUAGE']
-                
+
     def __setattr__(self, attribute, value):
         '''Override the __setattr__ method to register the default language'''
-        
+
         # Is the given attribute the default language?
         if attribute == 'default_language':
-            
+
             # Get the given language code
             language_code = LanguageManager.get_language(value)
-            
+
             # Is the given language code valid?
             if not language_code is None:
-                
+
                 # Loop through all strings
                 for key in self:
-                    
+
                     # Set the default language to use for that string
                     self[key]._default_language = language_code
-                    
+
                 # Override the given value
                 value = language_code
-                
+
         # Set the attribute
         super(LangStrings, self).__setattr__(attribute, value)
-        
+
     def _create_server_file(self):
         '''Creates a server specific langstrings file'''
 
@@ -172,36 +173,37 @@ class LangStrings(dict):
         '''Fixes all double escaped strings'''
 
         # Loop through all matches
-        for escaped_match in set(_double_escaped_pattern.finditer(given_string)):
-            
+        for escaped_match in set(
+                _double_escaped_pattern.finditer(given_string)):
+
             # Get the match as a string
             matching_string = escaped_match.group()
-            
+
             # Get a dictionnary of all groups
             matching_groups = escaped_match.groupdict()
-            
+
             # Are we matching any octal sequences?
             if matching_groups['octal']:
-                
+
                 # Replace it
-                given_string = given_string.replace(matching_string,
-                    chr(int(matching_groups['octal'])))
-                    
+                given_string = given_string.replace(
+                    matching_string, chr(int(matching_groups['octal'])))
+
             # Otherwise, are we matching any hexadecimal sequences?
             elif matching_groups['hexadecimal']:
-                
+
                 # Replace it
-                given_string = given_string.replace(matching_string,
-                    str(unhexlify(matching_groups['hexadecimal']),
-                    encoding='ascii'))
-                    
+                given_string = given_string.replace(
+                    matching_string, str(unhexlify(
+                        matching_groups['hexadecimal']), encoding='ascii'))
+
             # Otherwise, that means we are matching a notation
             else:
-                
+
                 # Replace it
-                given_string = given_string.replace(matching_string,
-                    unicode_escape_decode(matching_string)[0])
-                    
+                given_string = given_string.replace(
+                    matching_string, unicode_escape_decode(matching_string)[0])
+
         # Return the replaced string
         return given_string
 
@@ -220,7 +222,10 @@ class TranslationStrings(dict):
         if language is None:
 
             # Return an empty string
-            return '' # raise an error silently here, in case looping through players
+            '''
+                Possibly raise an error silently here
+            '''
+            return ''
 
         # Get the message's Template instance
         message = Template(self[language])
@@ -251,13 +256,13 @@ class TranslationStrings(dict):
 
         # Is there any default language defined?
         if hasattr(self, '_default_language'):
-            
+
             # Is the default language available for that string?
             if self._default_language in self:
-                
+
                 # Return the default language to use
                 return self._default_language
-                
+
         # Is the server's fallback language in the dictionary?
         if LanguageManager.fallback in self:
 
