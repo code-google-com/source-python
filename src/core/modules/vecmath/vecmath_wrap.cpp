8e953dee10b75419ac7b025d110bfc01abf6bb3d
Ayuto <Ayuto@localhost>
2013-08-02 15:04:57 +0200

    - Overloaded CVector to Vector conversion - Removed CVector.get_vector() - Replaced spaces by tabs

--- src/core/modules/vecmath/vecmath_wrap.cpp
+++ src/core/modules/vecmath/vecmath_wrap.cpp
@@ -29,235 +29,240 @@
 
 CVector::CVector(float x, float y, float z)
 {
-    m_Vector = Vector(x, y, z);
+	m_Vector = Vector(x, y, z);
 }
 
 CVector::CVector()
 {
-    m_Vector = Vector();
+	m_Vector = Vector();
 }
 
 CVector::CVector(Vector vec)
 {
-    m_Vector = vec;
+	m_Vector = vec;
 }
 
-Vector CVector::get_vector()
+CVector::operator Vector()
 {
-    return m_Vector;
+	return m_Vector;
+}
+
+CVector::operator Vector() const
+{
+	return m_Vector;
 }
 
 float CVector::get_x()
 {
-    return m_Vector.x;
+	return m_Vector.x;
 }
 
 void CVector::set_x(float fValue)
 {
-    m_Vector.x = fValue;
+	m_Vector.x = fValue;
 }
 
 float CVector::get_y()
 {
-    return m_Vector.y;
+	return m_Vector.y;
 }
 
 void CVector::set_y(float fValue)
 {
-    m_Vector.y = fValue;
+	m_Vector.y = fValue;
 }
 
 float CVector::get_z()
 {
-    return m_Vector.z;
+	return m_Vector.z;
 }
 
 void CVector::set_z(float fValue)
 {
-    m_Vector.z = fValue;
+	m_Vector.z = fValue;
 }
 
 float CVector::get_item(int iIndex)
 {
-    if (iIndex == 0)
-        return get_x();
-    else if (iIndex == 1)
-        return get_y();
-    else if (iIndex == 2)
-        return get_z();
-    else
-        BOOST_RAISE_EXCEPTION(PyExc_IndexError, "Index out of range.")
+	if (iIndex == 0)
+		return get_x();
+	else if (iIndex == 1)
+		return get_y();
+	else if (iIndex == 2)
+		return get_z();
+	else
+		BOOST_RAISE_EXCEPTION(PyExc_IndexError, "Index out of range.")
 
-    return NULL;
+	return NULL;
 }
 
 void CVector::set_item(int iIndex, float fValue)
 {
-    if (iIndex == 0)
-        return set_x(fValue);
-    else if (iIndex == 1)
-        return set_y(fValue);
-    else if (iIndex == 2)
-        return set_z(fValue);
+	if (iIndex == 0)
+		return set_x(fValue);
+	else if (iIndex == 1)
+		return set_y(fValue);
+	else if (iIndex == 2)
+		return set_z(fValue);
 
-    BOOST_RAISE_EXCEPTION(PyExc_IndexError, "Index out of range.")
+	BOOST_RAISE_EXCEPTION(PyExc_IndexError, "Index out of range.")
 }
 
 bool CVector::is_valid()
 {
-    return m_Vector.IsValid();
+	return m_Vector.IsValid();
 }
 
 void CVector::invalidate()
 {
-    m_Vector.Invalidate();
+	m_Vector.Invalidate();
 }
 
 void CVector::random(float fMin, float fMax)
 {
-    m_Vector.Random(fMin, fMax);
+	m_Vector.Random(fMin, fMax);
 }
 
 void CVector::zero()
 {
-    m_Vector.Zero();
+	m_Vector.Zero();
 }
 
 bool CVector::operator==(CVector* pOther)
 {
-    return m_Vector == pOther->get_vector();
+	return m_Vector == *pOther;
 }
 
 bool CVector::operator!=(CVector* pOther)
 {
-    return m_Vector != pOther->get_vector();
+	return m_Vector != *pOther;
 }
 
 CVector* CVector::operator+(object value)
 {
-    if (strcmp(extract<char*>(value.attr("__class__").attr("__name__")), "CVector") == 0)
-    {
-        CVector* pOther = extract<CVector*>(value);
-        return new CVector(m_Vector + pOther->get_vector());
-    }
+	if (strcmp(extract<char*>(value.attr("__class__").attr("__name__")), "CVector") == 0)
+	{
+		CVector other = extract<CVector>(value);
+		return new CVector(m_Vector + other);
+	}
 
-    float fValue = extract<float>(value);
+	float fValue = extract<float>(value);
 
-    // CS:GO SDK does not define Vector::operator+(float fValue), so we make it easy
-    // and create a new vector
-    return new CVector(m_Vector + CVector(fValue, fValue, fValue).get_vector());
+	// CS:GO SDK does not define Vector::operator+(float fValue), so we make it easy
+	// and create a new vector
+	return new CVector(m_Vector + CVector(fValue, fValue, fValue));
 }
 
 CVector* CVector::operator-(object value)
 {
-    if (strcmp(extract<char*>(value.attr("__class__").attr("__name__")), "CVector") == 0)
-    {
-        CVector* pOther = extract<CVector*>(value);
-        return new CVector(m_Vector - pOther->get_vector());
-    }
+	if (strcmp(extract<char*>(value.attr("__class__").attr("__name__")), "CVector") == 0)
+	{
+		CVector other = extract<CVector>(value);
+		return new CVector(m_Vector - other);
+	}
 
-    float fValue = extract<float>(value);
+	float fValue = extract<float>(value);
 
-    // CS:GO SDK does not define Vector::operator+(float fValue), so we make it easy
-    // and create a new vector
-    return new CVector(m_Vector - CVector(fValue, fValue, fValue).get_vector());
+	// CS:GO SDK does not define Vector::operator+(float fValue), so we make it easy
+	// and create a new vector
+	return new CVector(m_Vector - CVector(fValue, fValue, fValue));
 }
 
 CVector* CVector::operator*(object value)
 {
-    if (strcmp(extract<char*>(value.attr("__class__").attr("__name__")), "CVector") == 0)
-    {
-        CVector* pOther = extract<CVector*>(value);
-        return new CVector(m_Vector * pOther->get_vector());
-    }
+	if (strcmp(extract<char*>(value.attr("__class__").attr("__name__")), "CVector") == 0)
+	{
+		CVector other = extract<CVector>(value);
+		return new CVector(m_Vector * other);
+	}
 
-    float fValue = extract<float>(value);
-    return new CVector(m_Vector * fValue);
+	float fValue = extract<float>(value);
+	return new CVector(m_Vector * fValue);
 }
 
 CVector* CVector::operator/(object value)
 {
-    if (strcmp(extract<char*>(value.attr("__class__").attr("__name__")), "CVector") == 0)
-    {
-        CVector* pOther = extract<CVector*>(value);
-        return new CVector(m_Vector / pOther->get_vector());
-    }
+	if (strcmp(extract<char*>(value.attr("__class__").attr("__name__")), "CVector") == 0)
+	{
+		CVector other = extract<CVector>(value);
+		return new CVector(m_Vector / other);
+	}
 
-    float fValue = extract<float>(value);
-    return new CVector(m_Vector / fValue);
+	float fValue = extract<float>(value);
+	return new CVector(m_Vector / fValue);
 }
 
 void CVector::negate()
 {
-    m_Vector.Negate();
+	m_Vector.Negate();
 }
 
 float CVector::get_length()
 {
-    return m_Vector.Length();
+	return m_Vector.Length();
 }
 
 float CVector::get_length_sqr()
 {
-    return m_Vector.LengthSqr();
+	return m_Vector.LengthSqr();
 }
 
 bool CVector::is_zero(float fTolerance /* = 0.01f */)
 {
-    return m_Vector.IsZero(fTolerance);
+	return m_Vector.IsZero(fTolerance);
 }
 
 float CVector::normalize()
 {
-    return m_Vector.NormalizeInPlace();
+	return m_Vector.NormalizeInPlace();
 }
 
 bool CVector::is_within_box(CVector* pMins, CVector* pMaxs)
 {
-    return m_Vector.WithinAABox(pMins->get_vector(), pMaxs->get_vector());
+	return m_Vector.WithinAABox(*pMins, *pMaxs);
 }
 
 float CVector::get_distance(CVector* pOther)
 {
-    return m_Vector.DistTo(pOther->get_vector());
+	return m_Vector.DistTo(*pOther);
 }
 
 float CVector::get_distance_sqr(CVector* pOther)
 {
-    return m_Vector.DistToSqr(pOther->get_vector());
+	return m_Vector.DistToSqr(*pOther);
 }
 
 void CVector::mul_add(CVector* pA, CVector* pB, float fScalar)
 {
-    m_Vector.MulAdd(pA->get_vector(), pB->get_vector(), fScalar);
+	m_Vector.MulAdd(*pA, *pB, fScalar);
 }
 
 float CVector::dot(CVector* pOther)
 {
-    return m_Vector.Dot(pOther->get_vector());
+	return m_Vector.Dot(*pOther);
 }
 
 float CVector::get_length_2D()
 {
-    return m_Vector.Length2D();
+	return m_Vector.Length2D();
 }
 
 float CVector::get_length_2D_sqr()
 {
-    return m_Vector.Length2DSqr();
+	return m_Vector.Length2DSqr();
 }
 
 CVector* CVector::cross(CVector* pOther)
 {
-    return new CVector(m_Vector.Cross(pOther->get_vector()));
+	return new CVector(m_Vector.Cross(*pOther));
 }
 
 CVector* CVector::get_min(CVector* pOther)
 {
-    return new CVector(m_Vector.Min(pOther->get_vector()));
+	return new CVector(m_Vector.Min(*pOther));
 }
 
 CVector* CVector::get_max(CVector* pOther)
 {
-    return new CVector(m_Vector.Max(pOther->get_vector()));
+	return new CVector(m_Vector.Max(*pOther));
 }
